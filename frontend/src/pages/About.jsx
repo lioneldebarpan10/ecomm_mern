bf4b7ff613a5e3bb238242d514da6e8b999c9090
Debarpan Deb <dbrpn07cse@gmail.com>
2026-02-19 04:15:32 +0530

    About & Contact Page Added - Bugs in Navigation

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
-
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsLetter from '../components/NewsLetter'
 const About = () => {
   return (
     <div>
-      
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'About'} text2={'Us'} />
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img src={assets.about_img} alt="about-image" className='w-full md:max-w-[450px]' />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur, culpa quo! Autem sint ex, recusandae iusto officiis necessitatibus commodi fugit? Officia laudantium est cum, perspiciatis nulla dignissimos velit, harum inventore eos sapiente quos eius nam omnis ullam ea, blanditiis esse.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eos, explicabo dicta nobis porro minus cumque provident laudantium sunt voluptatum repudiandae? Corrupti fuga placeat quae rem recusandae esse autem temporibus, blanditiis, sapiente, officiis mollitia expedita officia! Consectetur provident nam ratione.</p>
+
+          <b className='text-gray-800'>Our Mission</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure eius aspernatur temporibus consectetur accusantium ad ipsum, minima odit est natus nostrum cupiditate ipsa culpa! Animi maiores quos rerum est enim?</p>
+        </div>
+      </div>
+
+
+      <div className='text-2xl py-4'>
+        <Title text1={'Why'} text2={'choose us'} />
+      </div>
+      <div className='flex flex-col md:flex-row mb-20 text-sm'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance</b>
+          <p className='text-gray-600'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur delectus quod harum? Aliquam, ipsam quis!</p>
+        </div>
+
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Convenience</b>
+          <p className='text-gray-600'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur delectus quod harum? Aliquam, ipsam quis!</p>
+        </div>
+
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional Customer Service</b>
+          <p className='text-gray-600'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur delectus quod harum? Aliquam, ipsam quis!</p>
+        </div>
+      </div>
+      <NewsLetter />
+
     </div>
+
+
   )
 }
 
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import Title from '../components/Title'
+import {assets} from '../assets/assets'
+import NewsLetter from '../components/NewsLetter'
 const Contact = () => {
   return (
     <div>
+      <div className='text-center text-2xl border-t pt-10'>
+        <Title text1={'Contact'} text2={'us'}/>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row justify-center gap-10 mb-28'>
+        <img src= {assets.contact_img} alt="contact-image" className='w-full md:max-w-[480px]'/>
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-gray-600 text-xl'>Our Store</p>
+          <p className='text-gray-500'>735101, Jalpaiguri <br />West Begal, India</p>
+          <p className='text-gray-500'>Tel: +8617374285 <br />dbrpn07cse@gmail.com</p>
+          <p className='text-gray-600 text-xl font-semibold'>Careers at Forever</p>
+          <p className='text-gray-500'>Learn more about our teams and job openings</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500 cursor-pointer'>Explore Jobs</button>
+        </div>
+      </div>
+      <NewsLetter />
       
     </div>
   )
